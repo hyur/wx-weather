9d1b2e557121d80aa3b76db05a09492dd791ada3
lxin <xlxin68@foxmail.com>
2018-04-12 15:00:42 +0800

    实现构建天气列表功能

--- README.md
+++ README.md
@@ -1,52 +1,10 @@
-# wx-weather
-分支1-1提交内容：
-4、rpx（responsive pixel）可以根据屏幕宽度进行自适应，规定任何手机屏幕宽度为750rpx
+3-1构建列表
 
-	* iphone7的宽高比是375*667，因此屏幕宽度对应667/375*750rpx = 1334rpx
-	* iPhone 7 Plus 宽高比 414 x 736，因此屏幕高度对应与 736 / 414 x 750 rpx = 1333.33 rpx
-	* iPhone X 宽高比 375 x 812，因此屏幕高度对应与 812 / 375 x 750 rpx = 1624 rpx   
+16、如何创建多个并列视图？
+
+	* <view wx:for="{{[1,2,3,4,5]}}">{{item}}</view>
 
 
-5、比较分支的修改（在示范代码中查看）
-
-	* 如果未切换到某个分支，你可以使用git diff origin/1-1 origin 2-1来比较不同
-	* 也可以先git checkout 2-1分支后，在查看两个分支的不同git diff 1-1 2-1  
-
-
-6、通常来说，行高是字体的1.4倍
-7、修改微信小程序标题栏颜色在app.json中查找navigationBarBackgroundColor  
-
-分支2-1提交内容：
-8、小程序启动的时候，会首先调用启动页面的onLoader()函数，作为Page()函数其中的一个参数。
-9、获取真实的天气数据流程：
-
-	* 调用天气API获取json数据
-	* 解析json数据并展示
-	* 下拉刷新
-
-10、天气api的查看地址是https://github.com/udacity/cn-wechat-weather/blob/default-1-1/weather_api.md
-        天气api的请求地址是：https://test-miniprogram.com/api/weather/now?city=%E4%B8%8A%E6%B5%B7%E5%B8%82
-    URL的组成:
-
-	* 协议。它可以是HTTP(不带SSL)或HTTPS(带SSL)
-	* 主机。例如test-miniprogram.com
-	* 路径。例如/api/weather/now
-	* 查询字符串。规则为？后显示参数查询值，如？city=上海市
-
-
-11、JSON扁平化的代码可以通过在线网站格式化，推荐网站为https://codebeautify.org/jsonviewer   
-12、bug解决：wx.request()提交请求的时候出现“不在以下 request 合法域名列表中”
-
-	* 解决方法：微信平台---》设置----》开发设置------->服务器配置----->bug解决
-
-
-13、微信发送请求使用wx.request()具体使用方式可查看api
-        wx.request保证了API返回的所有数据都存储在res.data中
-14、WXHL中的动态数据都来自于Paga中的data
-15、实现下拉刷新功能：
-
-	* 需要在config的window选项中开启 enablePullDownRefresh
-	* 在Page函数中调用onPullDownRefresh()函数，请求天气API
-	* 数据返回后，在compolete()函数中调用停止刷新wx.stopPullDownRefresh()函数
-	* 把公共功能getNow()提取（请求天气API）出来的时候，onLoad()函数也调用了停止刷新。解决办法是：给getNow添加回调参数如getNow（callback），在complete函数中判断回调函数若存在则执行。
-
+17、实现横向滑动的列表，使用scroll-view组件
+18、获取当前时间，并对时间进行运算
+19、代码重构
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,24 +1,25 @@
 const weatherMap={
   'sunny':'晴天',
-  'clody':'多云',
+  'cloudy':'多云',
   'overcast':'阴',
   'lightrain': '小雨',
   'heavyrain': '大雨',
   'snow': '雪'
 }
 const weatherColor={
-  'sunny': '#ff0',
-  'clody': '#cbeefd',
-  'overcast': '#cbeefd',
-  'lightrain': '#cbeefd',
-  'heavyrain': '#cbeefd',
-  'snow': '#cbeefd'
+  'sunny': '#cbeefd',
+  'cloudy': '#deeef6',
+  'overcast': '#c6ced2',
+  'lightrain': '#bdd5e1',
+  'heavyrain': '#c5ccd0',
+  'snow': '#aae1fc'
 }
 Page({
   data:{
     nowTemp:2,
     nowWeather:'',
-    nowWeatherBg:''
+    nowWeatherBg:'',
+    forecast:''
   },
   // 下拉刷新
   onPullDownRefresh(){
@@ -40,22 +41,42 @@ Page({
       },
       success: res => {
         let result = res.data.result;
-        let temp = result.now.temp;
-        let weather = result.now.weather;
-        this.setData({
-          nowTemp: temp,
-          nowWeather: weatherMap[weather],
-          nowWeatherBg: '/images/' + weather + '_bg.png'
-        });
-        wx.setNavigationBarColor({
-          frontColor: '#ffffff',
-          backgroundColor: weatherColor[weather],
-        })
+        this.setNow(result);
+        this.setHourly(result);   
       },
       complete:()=>{
         // 如果存在回调函数则执行，不存在则不执行
         callback&&callback();
       }
+    })
+  },
+  // 设置当前的时间
+  setNow(result){
+    let temp = result.now.temp;
+    let weather = result.now.weather;
+    this.setData({
+      nowTemp: temp,
+      nowWeather: weatherMap[weather],
+      nowWeatherBg: '/images/' + weather + '-bg.png', 
+    });
+    wx.setNavigationBarColor({
+      frontColor: '#ffffff',
+      backgroundColor: weatherColor[weather],
+    })
+  },
+  // 设置未来24小时的时间
+  setHourly(result){
+    let forecast = result.forecast;
+    let currentTime = new Date().getHours();
+    for (let i = 0; i < 8; i++) {
+      forecast[i].timeValue = i == 0 ? "现在" : currentTime + "时";
+      currentTime += 3;
+      if (currentTime > 24) {
+        currentTime -= 24;
+      }
+    }
+    this.setData({
+      forecast
     })
   }
 })
